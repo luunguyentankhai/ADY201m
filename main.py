e2c866c6845378e5002b7ccc92308e2f205c6839
luunguyentankhai <tankhai15406@gmail.com>
2026-05-13 21:31:33 +0700

    something on main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from src.Collect_and_Process import collection
+from src.Collect_and_Process import collection,Process
 
 class Main:
     def __init__(self):
         self.CL = collection.Data_Collection()
+        self.PC = Process
 
 if __name__ == "__main__":
     main = Main()
-    main.CL.Collection()
+    # main.CL.Collection()
+    # main.PC.Pre().Read_info()
+    # print(f"{'='*50}")
+    # main.PC.Pre().Imbalance_Check()
+    main.PC.Pre().Normalization()
 
+# TODO: someone nhận trách nhiệm làm cái schema.sql đi tao sẽ nghĩ cách để đẩy data:>>
+# TODO: nếu ok thì làm luôn phần đẩy kết nối python với sql
+# TODO: code sẽ ở phần src là chính nha

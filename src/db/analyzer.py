cec11db9b052e81b8484ee87ad880a15de9016c0
luunguyentankhai <tankhai15406@gmail.com>
2026-05-25 20:12:22 +0700

    new: file to manager and call sql file

--- src/db/analyzer.py
+++ src/db/analyzer.py
@@ -1,12 +1,17 @@
 import pandas as pd
 from src.config import db_config, dir_config
 from sqlalchemy import text
+from src.config.logs_config import setup_log, auto_logger
+
+logger = setup_log(name="SQL_queries", filename="DataBase")
+
 
 class FraudAnalyzer:
     def __init__(self):
         self.engine = db_config.db_engine()
-        self.sql_dir = dir_config.sql_dir
-
+        self.sql_dir = dir_config.sql_dir 
+    
+    @auto_logger(logger)
     def _execute_sql(self, filename):
         file_path = self.sql_dir / filename
 
@@ -19,26 +24,46 @@ class FraudAnalyzer:
             df = pd.read_sql(text(query), con=self.engine)
             return df
         except Exception as e:
-            print(f"ERROR: {e}\n {filename}")
+            logger.error(f"ERROR: {e}\n {filename}")
             return None
 
+    @auto_logger(logger)
+    def _execute_ddl(self, filename):
+        file_path = self.sql_dir / filename
+
+        if not file_path.exists():
+            raise FileNotFoundError(f"SQL file missing: {file_path}")
+
+        query = file_path.read_text(encoding="utf-8")
+
+        try:
+            with self.engine.begin() as conn:
+                conn.execute(text(query))
+            logger.info(f"Successfully executed DDL script: {filename}")
+            return True
+        except Exception as e:
+            logger.error(f"Failed to execute DDL {filename}: {e}")
+            return False
+    
+    @auto_logger(logger)
     def get_fraud_rate(self):
-        print(f"Analyzing fraud rate")
+        logger.info(f"Analyzing fraud rate")
         return self._execute_sql("get_fraud_rate.sql")
 
+    @auto_logger(logger)
     def get_anomalies(self):
-        print(f"Scanning for anomalous transactions")
+        logger.info(f"Scanning for anomalous transactions")
         return self._execute_sql("get_anomalies.sql")
 
+    @auto_logger(logger)
     def get_ml_feature(self):
-        print(f"Fetching Feature Table for ML")
+        logger.info(f"Fetching Feature Table for ML")
 
-        df_feature = self._execute_sql("get_feature.sql")
-        if df_feature is not None:
-            return df_feature
-        return df_feature
+        return self._execute_ddl("get_feature.sql")
+         
 
+    @auto_logger(logger)
     def get_fraud_patterns(self):
-        print(f"Detecting recurring fraud patterns")
+        logger.info(f"Detecting recurring fraud patterns")
         return self._execute_sql("get_patterns.sql")
 

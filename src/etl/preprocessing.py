e70a06fa2346e6d73c68d860d208e76e598421a7
luunguyentankhai <tankhai15406@gmail.com>
2026-05-28 22:22:44 +0700

    change: update preprocessing.py to splitter, undersampler and scale data

--- src/etl/preprocessing.py
+++ src/etl/preprocessing.py
@@ -1,26 +1,75 @@
-# TODO: sửa lại cái file preprocessing nha cái này để tạm thôi
+import joblib
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from src.config.dir_config import Models_Dir
+from src.config.logs_config import setup_log, auto_logger
 
-def Normalization(self):
-        self.NA_Check()
-        # Norm TimeStamp 
-        self.df['Timestamp'] = pd.to_datetime(self.df['Timestamp'])
+logger = setup_log(name="Data_Preprocessing", filename="app")
 
-        self.df['Transaction_Hour'] = self.df['Timestamp'].dt.hour
-        self.df['Transaction_DayOfWeek'] = self.df['Timestamp'].dt.dayofweek
+class DataSplitter:
+    def __init__(self, target_col='is_fraud', random_state=42):
+        self.target_col = target_col
+        self.random_state = random_state
 
-        self.df = self.df.drop('Timestamp', axis=1)
+    @auto_logger(logger)
+    def split_train_val_test(self, df):
+        """Executes a 70/15/15 split for Train, Validation, and Test sets."""
+        logger.info("Executing Data Splitter. Dropping 'step' column as per Data Contract.")
+        if 'step' in df.columns:
+            X = df.drop(columns=['step'])
+        else:
+            X = df.copy()
 
-        # Norm Device_type and Location using One-Hot Encoding
-        self.df = pd.get_dummies(self.df, columns=['Device_Type','Location'], drop_first=True)
+        y = X.pop(self.target_col)
+        
+        X_temp, X_test, y_temp, y_test = train_test_split(
+            X, 
+            y, 
+            test_size=0.15, 
+            stratify=y, 
+            random_state=self.random_state
+        )
 
-        # Norm Transaction_Amount and Daily_Transaction_Count
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_temp, 
+            y_temp, 
+            test_size=0.1765, 
+            stratify=y_temp, 
+            random_state=self.random_state
+        )
 
-        scaler = StandardScaler()
+        return X_train, X_val, X_test, y_train, y_val, y_test
 
-        numberical_cols = ['Transaction_Amount', 'Daily_Transaction_Count']
+    @auto_logger(logger)
+    def smart_undersample(self, X_train, y_train, target_normal_size=1_000_000):
+        """Reduces majority class while preserving 100% of minority class."""
+        X_fraud, y_fraud = X_train[y_train == 1], y_train[y_train == 1]
+        X_normal, y_normal = X_train[y_train == 0], y_train[y_train == 0]
+        
+        X_normal_sampled = X_normal.sample(
+                n=target_normal_size, 
+                random_state=self.random_state)
+        y_normal_sampled = y_normal.sample(
+                n=target_normal_size, 
+                random_state=self.random_state)
+        
+        X_train_sampled = pd.concat([X_fraud, X_normal_sampled])
+        y_train_sampled = pd.concat([y_fraud, y_normal_sampled])
+        return X_train_sampled, y_train_sampled
 
-        self.df[numberical_cols] = scaler.fit_transform(self.df[numberical_cols])
+class FeatureScaler:
+    def __init__(self):
+        self.scaler = StandardScaler()
 
-        print(self.df.head(5))
-
-        return self.df
+    @auto_logger(logger)
+    def scale_and_save(self, X_train, X_val, X_test):
+        """Fits StandardScaler on Train, transforms all splits, and saves to disk."""
+        X_train_scaled = self.scaler.fit_transform(X_train)
+        X_val_scaled = self.scaler.transform(X_val)
+        X_test_scaled = self.scaler.transform(X_test)
+        
+        scaler_file = Models_Dir / "fraud_scaler.pkl"
+        joblib.dump(self.scaler, scaler_file)
+        
+        return X_train_scaled, X_val_scaled, X_test_scaled
